d847317c706e5d935a305784d2435bca0eb35264
Carlos E. Valenzuela C <89716956+CarlosVlaCor@users.noreply.github.com>
2023-05-22 09:59:52 -0700

    Cambios

--- eventeam/applications/salones/views.py
+++ eventeam/applications/salones/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-# Create your views here.
 class ListaSalonesView(TemplateView):
     template_name = 'salones/lista-salones.html'
 
